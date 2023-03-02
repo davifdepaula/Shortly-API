e61558c9b15d28b88cf6cf5d9177f2f8075b892a
davifdepaula <davi.ferreira100@gmail.com>
2023-03-02 08:38:00 -0300

    feat: get user/me

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -30,18 +30,38 @@ const signIn = async(req, res) => {
   }
 }
 
+const getUser = async(req, res) => {
+  try {
+    const {userId} = res.locals.session
+
+    const userDateInfo = await db.query(`SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount"
+    FROM users JOIN urls on users.id = urls."userId"  
+      WHERE users.id = $1;`, [userId])
+
+    const visitCount = await db.query(`SELECT users.name, sum(urls."visitCount")
+    FROM users JOIN urls on users.id = urls."userId"  
+      WHERE urls."userId" = $1 group by users.name`, [userId])
+
+    const name = visitCount.rows[0].name
+    const id = userId
+    const shortenedUrls = userDateInfo.rows
+    return res.send({id, name, visitCount: visitCount.rows[0].sum, shortenedUrls})
+    
+  } catch (error) {
+    return res.send(error).status(500)
+  }
+}
+
 const urlShorten = async(req, res) => {
   try {
     const {session, url} = res.locals
     const shortUrl = nanoid(10)
     await db.query(`INSERT INTO urls 
-      ("userId", url, "shortUrl") values ($1, $2, $3)`, [session.id, url, shortUrl])
-    
+      ("userId", url, "shortUrl") values ($1, $2, $3)`, [session.id, url, shortUrl])    
     const urlData = await db.query(`SELECT * FROM urls 
       WHERE "shortUrl" = $1`, [shortUrl])
     const id = urlData.rows[0].id
     return res.status(201).send({id: id, shortUrl: shortUrl})
-
   } catch (error) {
     return res.send(error).status(500)
   }
@@ -51,14 +71,14 @@ const deleteById = async(req, res) => {
   try {
     const {urlId} = res.locals
     await db.query(`DELETE FROM urls WHERE id = $1`, [urlId])
-    return res.sendStatus(204)
-    
+    return res.sendStatus(204)    
   } catch (error) {
     return res.send(error).status(500)
   }
 }
 
 export {
+  getUser,
   signUp,
   signIn,
   urlShorten,
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { deleteById, signIn, signUp, urlShorten } from '../controllers/userControllers.js'
+import { deleteById, getUser, signIn, signUp, urlShorten } from '../controllers/userControllers.js'
 import { checkUserId, signInValidate, signUpValidate, tokenValidation, urlValidate } from '../middleware/authValidation.js'
 
 const userRoutes = express.Router()
-
+userRoutes.get('/users/me', tokenValidation, getUser)
 userRoutes.post('/signup', signUpValidate, signUp)
 userRoutes.post('/signin', signInValidate, signIn)
 userRoutes.post('/urls/shorten', tokenValidation, urlValidate, urlShorten)
