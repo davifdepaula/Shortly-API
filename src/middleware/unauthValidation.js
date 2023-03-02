2da699112189ed61aa01483226c4999c64307899
davifdepaula <davi.ferreira100@gmail.com>
2023-03-02 06:52:29 -0300

    fix: try return error when get by id dont have a url

--- src/middleware/unauthValidation.js
+++ src/middleware/unauthValidation.js
@@ -8,7 +8,7 @@ const validateUrlId = async(req, res, next) => {
     if (checkId.rowCount === 0) return res.sendStatus(404)
     res.locals.id = checkId.rows[0].id
     res.locals.shortUrl = checkId.rows[0].shortUrl
-    if (checkId.rows[0].url.value.url) res.locals.url = checkId.rows[0].value.url
+    if (checkId.rows[0].url.value) res.locals.url = checkId.rows[0].value.url
     else res.locals.url = checkId.rows[0].url
     next()
   } catch (error) {
